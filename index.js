6dd29e07ce61fab116f29b792f7a77c98bc408b6
Jan Sevilla <blastersevilla@hotmail.com>
2023-03-12 15:19:49 -0400

    alert function added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 let listItems = document.querySelector('.divAddList');
-
+let arrayItems = [];
 
 
 function addItem(){
     let li = document.createElement('p');
     li.innerHTML = 'test';
+    arrayItems.push(li.innerText)
     listItems.appendChild(li);
+    console.log(arrayItems);
+}
+
+function alertFunction(){
+    alert(arrayItems)
 }
 
 addItem();
+alertFunction();
