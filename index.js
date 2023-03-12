0111c853ccff32d97d8f758477327228ece0718d
Jan Sevilla <blastersevilla@hotmail.com>
2023-03-12 15:44:43 -0400

    added input box and add input button

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ let listItems = document.querySelector('.divAddList');
 let arrayItems = [];
 let alertBtn = document.querySelector('.alertButton');
 
+let inputB = document.querySelector('.inputBox');
+let inputAdd = document.querySelector('.addBtn');
+
+inputAdd.addEventListener('click', addItem);
+
 alertBtn.addEventListener('click', alertFunction);
 
 function addItem(){
-    let li = document.createElement('p');
-    li.innerHTML = 'test';
-    arrayItems.push(li.innerText)
-    listItems.appendChild(li);
-    console.log(arrayItems);
+    let li = document.createElement('p'); 
+    if(inputB.value !== "")
+    {
+        li.innerHTML = inputB.value;
+        arrayItems.push(li.innerText)
+        listItems.appendChild(li);
+        console.log(arrayItems);
+    }else{
+        alert("Try adding text");
+    }
 }
 
 function alertFunction(){
-    alert(arrayItems)
-}
+    if(inputB.value !== "")
+    {
+        alert(arrayItems)
+    }else{
+        alert("Try adding text");
+    }
 
-addItem();
+}
